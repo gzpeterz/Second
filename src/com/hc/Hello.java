0dd60502e78c4eb52dca9b5a00d3a7778d52f3d9
your <501127414@qq.com>
2017-10-27 14:38:32 +0800

    随便玩一下

--- src/com/hc/Hello.java
+++ src/com/hc/Hello.java
@@ -12,6 +12,12 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("this is aa test");
 		System.out.println("this is bbbb test");
+		
+		/**
+		 * @author zhangweihang
+ 		*
+ 		*/
+		System.out.println("hello everybody");
 
 	}
 }
